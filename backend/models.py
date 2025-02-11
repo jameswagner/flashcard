4d1926a4ce14b48d78a3522d92e75b8f9ff715bc
jameswagner <jamesrwagner@gmail.com>
2025-02-11 10:32:35 -0600

    fix schema issue

--- backend/models.py
+++ backend/models.py
@@ -15,7 +15,6 @@ flashcard_set_association = Table(
 
 class FlashcardSet(Base):
     __tablename__ = "flashcard_sets"
-    __table_args__ = {'schema': 'flashcards'}
     
     id = Column(Integer, primary_key=True)
     title = Column(String(255), nullable=False)
